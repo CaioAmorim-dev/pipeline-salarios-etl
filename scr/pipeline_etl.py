4fbc2717f699693237af52e8a6c20858e11d4404
13668497443 <caio13483@gmail.com>
2025-10-15 15:22:34 -0300

    feat: Estrutura inicial do projeto e importação de dados

---
+++ scr/pipeline_etl.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+
+# 1. Carregamento dos dados
+df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+
