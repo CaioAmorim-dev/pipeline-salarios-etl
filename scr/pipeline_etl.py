32d0a18b783a0b86eef51c18022ec9368673fd66
13668497443 <caio13483@gmail.com>
2025-10-15 15:25:39 -0300

    feat: Renomeia colunas da b ase de dados

--- scr/pipeline_etl.py
+++ scr/pipeline_etl.py
@@ -8,3 +8,18 @@ import plotly.express as px
 # 1. Carregamento dos dados
 df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
 
+# 2. Renomeando colunas
+df.rename(columns={
+    'work_year': 'ano_trabalho',
+    'experience_level': 'nivel_experiencia',
+    'employment_type': 'tipo_emprego',
+    'job_title': 'cargo',
+    'salary': 'salario',
+    'salary_currency': 'moeda',
+    'salary_in_usd': 'usd',
+    'employee_residence': 'residencia',
+    'remote_ratio': 'remoto',
+    'company_location': 'localizacao_empresa',
+    'company_size': 'porte_empresa'
+}, inplace=True)
+
